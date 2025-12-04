fc8b6de657b8716f508f3db6ec25074e53cee54f
Budimir1 <104171611+Budimir1@users.noreply.github.com>
2025-12-04 15:43:27 +0300

    Commit

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,81 +1,98 @@
 package ticTacToe;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static boolean gameOver = false;
+
+    public static boolean isWin = false;
     public static int num;
 
-    public static void statusGame(){
+    public static void checkingForMatch (char[][] field) {
 
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][0] == field[i][1] && field[i][0] == field[i][2] || // сравнение по горизонтали
+                    field[0][i] == field[1][i] && field[0][i] == field[2][i] || // сравнение по вертикали
+                    field[0][0] == field[1][1] && field[0][0] == field[2][2] || // сравнение по
+                    field[2][0] == field[1][1] && field[2][0] == field[0][2]) { // диагоналям
+
+                isWin = true;
+                break;
+            }
+        }
     }
-
-    public static void queue(){
-
-    }
-    public static int inpurMan(){
-        Scanner th = new Scanner(System.in);
-        int num = th.nextInt();
-        return num;
-    }
-
-    public static void playField(String[][] filed){
-        for (int i = 0; i < filed.length; i++) {
+    public static void printField (char[][] field) {
+        for (int i = 0; i < field.length; i++) {
             System.out.print("|");
-            for (int j = 0; j < filed.length; j++) {
-                System.out.print(filed[i][j] + "|");
+            for (int j = 0; j < field.length; j++) {
+                System.out.print(field[i][j] + "|");
             }
             System.out.println();
         }
-
     }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int randomPlayer = (int) (Math.random() * 2 + 1);
+        if (randomPlayer == 1)
+            System.out.println("Player1 ходит первым!" + '\n');
+        else System.out.println("Player2 ходит первым!" + '\n');
+
+        System.out.println("Введите число от 1 до 9, чтобы сделать Ваш ход: " + '\n');
+
+        char[][] field = {{'1','2','3'},
+                          {'4','5','6'},
+                          {'7','8','9'}};
 
 
 
-    public static void main(String[] args) {
-
-        int randomPlayer = (int) ((Math.random() * 2 + 1));
-        if (randomPlayer % 2 == 1){
-            System.out.println("Игрок №1 ходит первый \n");
-        }else System.out.println("Игрок №2 ходит первый \n");
-
-        System.out.println("Для хода вы должны выбрать число от 1 до 9");
-        num = inpurMan();
-
-        String [][] filed = {{"1", "2", "3"},
-                             {"4", "5", "6"},
-                             {"7", "8", "9"}
-        };
-
-        String motion;
-
+        char player;
         int switcher = 0;
+        while (true) {
 
-        while (true){
-            if (switcher % 2 == 0){
-                motion = "x";
-            }else motion = "o";
+            if (switcher % 2 == 0)
+                player = 'x';
+            else player = 'o';
 
-            playField(filed);
+            printField(field);
 
-            switch (num){
-                case 1: filed[0][0] = motion; break;
-                case 2: filed[1][1] = motion; break;
-                case 3: filed[2][2] = motion; break;
-                case 4: filed[0][1] = motion; break;
-                case 5: filed[0][2] = motion; break;
-                case 6: filed[1][0] = motion; break;
-                case 7: filed[2][0] = motion; break;
-                case 8: filed[2][1] = motion; break;
-                case 9: filed[1][2] = motion; break;
+            try {
+                num = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Число, только число!");
+                continue;
             }
-//            try{
-//                num = Integer.parseInt(num);
-//            }catch (NumberFormatException e){
-//                System.out.println("Пожалуйста, введите число или 'exit'");
-//                continue;
-//            }
-        }
 
+            if (Arrays.deepToString(field).contains(Integer.toString(num)) && num <= 9 && num > 0) {
+                switch (num) {
+                    case 1: field[0][0] = player; break;
+                    case 2: field[0][1] = player; break;
+                    case 3: field[0][2] = player; break;
+                    case 4: field[1][0] = player; break;
+                    case 5: field[1][1] = player; break;
+                    case 6: field[1][2] = player; break;
+                    case 7: field[2][0] = player; break;
+                    case 8: field[2][1] = player; break;
+                    case 9: field[2][2] = player; break;
+                }
+            } else {
+                System.out.println("Введите число из оставшихся на поле!");
+                continue;
+            }
+
+            switcher++;
+
+            checkingForMatch(field);
+
+            if (isWin) {
+                System.out.println("YOU WIN!!!");
+                printField(field);
+                break;
+            }
+
+        }
     }
 }
