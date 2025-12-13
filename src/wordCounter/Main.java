6d70604077dd3884ffee578e035bc06ef58df4ec
Budimior <budimir_buliskeria@mail.ru>
2025-12-13 13:05:16 +0300

    Commit

---
+++ src/wordCounter/Main.java
@@ -0,0 +1,30 @@
+package wordCounter;
+
+import java.util.*;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String input = scanner.nextLine();
+        input = input.toLowerCase();
+        String[] word = input.split("\\W+");
+
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String Word : word) {
+            if (wordCount.containsKey(Word)) {
+                wordCount.put(Word, wordCount.get(Word) + 1);
+            }else {
+                wordCount.put(Word, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+    }
+}
